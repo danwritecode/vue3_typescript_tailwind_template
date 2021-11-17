db9002a1e5b6343926d2753513a4eca360c5315d
nelsynelz <dnelson4993@outlook.com>
2021-11-16 20:27:16 -0500

    basic setup, tailwind integration, simple composition API typescript example

---
+++ src/views/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="text-4xl font-black p-12">
+    {{ name }}
+    <div class="mt-10">
+      <button @click="changeName()" type="button" class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        Change Name
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+
+    const name = ref('Foo')
+    const age = ref<number | string>(25)
+
+    const changeName = () => {
+      name.value = 'Bar'
+    }
+    
+    return {
+      name,
+      age,
+      changeName
+    }
+  },
+})
+</script>
+
+
+<style>
+
+</style>
